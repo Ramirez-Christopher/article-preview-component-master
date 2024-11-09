aad14d2f23d2aa333c7c220e629cda2038e20852
Ramirez-Christopher <chris.ramirez@nevada.unr.edu>
2024-11-08 20:46:56 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.share-btn').addEventListener('click', function() {
+    const shareOptions = document.querySelector('.share-options');
+    const shareButton = document.querySelector('.share-btn');
+  
+  
+    shareOptions.style.display = shareOptions.style.display === 'none' || shareOptions.style.display === '' ? 'flex' : 'none';
+    
+  
+    shareButton.classList.toggle('active');
+  });
